3c911eebdaab99cbf1e308198f9c2857f041b857
jamesndang <88268832+jamesndang@users.noreply.github.com>
2021-08-09 00:28:02 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,7 +11,7 @@ class profile::ssh_server {
       ssh_authorized_key {'root@master.puppet.vm':
       ensure => present,
       user => 'root',
-      type => 'ssh-rsa'
+      type => 'ssh-rsa',
       key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDDnIGlV9Sp455/zQGrVWGVYSzDVqHleLZP64P3PT64A9KnKFqRiYJRgxvvlR0SZv808ssn+0WMABpWSdFvFArt9Pgtp/odaYO835IKl6bYyY+OZrjX0I1ql7mCu4dn6WVrT+BZRmg+W6UFycu+qd+zTf9aGHFalAO8yNYzNcAbZxvQOB2Ysjb5049B/BXBx/ffIgQK92BENw9FJ7vYE+bDRgMYMcmmeWrArmiTWxuP7lXiSvZa9M/yKKyuboDvxeuVePAHc7Z0EvlCOEEWVlSD/EWKlPMAVElTOnr1sfOYED0V2HM7fc6T9ckd+TV30yPGcQYPgQdeF1Nb+JBggc4b',
       }
 }
